678ad06e2d6459692bd319ca88e52fd7c90f06e6
yolao08 <00001101324028sp@al.educacao.sp.gov.br>
2024-11-22 11:44:44 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Musicais',
+    'Qual é o musical mais longo da história da Broadway?',
+    'O musical mais longo da história da Broadway é "The Phantom of the Opera". Ele estreou em 26 de janeiro de 1988 e encerrou sua temporada em 16 de abril de 2023, após 35 anos em cartaz e mais de 13.000 apresentações. '
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Séries',
+    'Qual é o nome do chefe na série The Office?',
+    'O chefe na série The Office é Michael Scott, interpretado por Steve Carell.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Esportes',
+    'Qual país venceu a Eurocopa de 2012?',
+    'A Espanha venceu a Eurocopa de 2012.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Animais',
+    'Qual é o maior animal terrestre do mundo?',
+    'O maior animal terrestre do mundo é o elefante-africano. Ele pode pesar até 7 toneladas e medir cerca de 3 a 4 metros de altura no ombro. Esses elefantes são conhecidos por suas enormes orelhas e presas de marfim.'
 )
